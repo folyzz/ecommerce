d425d326695b73d926f653dc668c8cc5eadc7adf
Ammar Badie <folyzz11@gmail.com>
2018-11-18 14:23:27 +0300

    global brands

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
     
     $('[data-toggle="tooltip"]').tooltip();
+
+    $('[data-add-to-cart]').click(function(e){
+        alert('أضيف المُنتج إلى عربة الشراء');
+        e.stopPropagation();
+    });
 });
